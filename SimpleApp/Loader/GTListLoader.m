5531152d60c4abd8f59bc9aa5dac957c16b6ad5c
Duan Zhu <duan.zhu@shopee.com>
2021-02-15 22:47:51 +0800

    integrate AFNetworking

--- SimpleApp/Loader/GTListLoader.m
+++ SimpleApp/Loader/GTListLoader.m
@@ -7,25 +7,34 @@
 //
 
 #import "GTListLoader.h"
+#import <AFNetworking.h>
 
 @implementation GTListLoader
 
 -(void)loadListData {
     
     NSString *urlString = @"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
-    NSURL *listURL = [NSURL URLWithString:urlString];
+//    NSURL *listURL = [NSURL URLWithString:urlString];
     
-    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    
-    NSURLSessionTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+    [[AFHTTPSessionManager manager] GET:urlString parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"");
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         NSLog(@"");
     }];
     
-    [dataTask resume];
-    
-    NSLog(@"");
+//    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+//
+//    NSURLSession *session = [NSURLSession sharedSession];
+//
+//    NSURLSessionTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//        NSLog(@"");
+//    }];
+//
+//    [dataTask resume];
+//
+//    NSLog(@"");
 }
 
 @end
