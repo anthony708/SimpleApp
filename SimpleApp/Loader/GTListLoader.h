049e89d755c6873017015c814e8698627201b7cb
Duan Zhu <duan.zhu@shopee.com>
2021-02-15 19:45:47 +0800

    add NSURLSession

---
+++ SimpleApp/Loader/GTListLoader.h
@@ -0,0 +1,21 @@
+//
+//  GTListLoader.h
+//  SimpleApp
+//
+//  Created by DuanZhu on 2021/2/15.
+//  Copyright © 2021 DuanZhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+/**
+ 列表请求
+ */
+@interface GTListLoader : NSObject
+
+-(void)loadListData;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SimpleApp/Loader/GTListLoader.m
@@ -0,0 +1,31 @@
+//
+//  GTListLoader.m
+//  SimpleApp
+//
+//  Created by DuanZhu on 2021/2/15.
+//  Copyright © 2021 DuanZhu. All rights reserved.
+//
+
+#import "GTListLoader.h"
+
+@implementation GTListLoader
+
+-(void)loadListData {
+    
+    NSString *urlString = @"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
+    NSURL *listURL = [NSURL URLWithString:urlString];
+    
+    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    NSURLSessionTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"");
+    }];
+    
+    [dataTask resume];
+    
+    NSLog(@"");
+}
+
+@end
